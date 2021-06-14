5b46aa28644be7799cac32b0f0def0ff1fa6e880
Madhavan Mani <maadhu97.madhavan@gmail.com>
2021-06-14 15:35:29 +0530

    Update upload.py

--- upload.py
+++ upload.py
@@ -3,16 +3,39 @@ import argparse
 
 
 
-url = 'http://192.168.65.132:8080/api/v2/import-scan/'
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Script for import scan results into defectdojo using API v2')
+    parser.add_argument('--host', help="Host eg: http://192.168.1.1 or https://defectdojo.com", required=True)
+    parser.add_argument('--api_key', help="API V2 Key", required=True)
+    parser.add_argument('--engagement_id', help="Engagement ID Mandatory", required=True)
+    parser.add_argument('--result', help="Scanner output file", required=True)
+    parser.add_argument('--scanner', help="Type of scanner", required=True)
+    parser.add_argument('--date_of_scan', help="Date of the scan in YYYY-MM-DD eg: 2021-05-25", required=True)
+    parser.add_argument('--build_id', help="Reference to external build id", required=False)
+
+    # Parse out arguments
+    args = vars(parser.parse_args())
+    host = args["host"]
+    api_key = args["api_key"]
+    result_file = args["result_file"]
+    scanner = args["scanner"]
+    engagement_id = args["engagement_id"]
+    date = args["date_of_scan"]
+
+
+
+
+
+url = host
 headers = {'Accept': 'application/json',
-           'Authorization': 'Token 97a73cd02f5643f9ff1847c6deb790f7382b0048'}
+           'Authorization': 'Token'+ api_key}
 
 
 
 
 
 
-files = {'file': open('bandit-output.json','rb')}
+files = {'file': open(result_file,'rb')}
 
 
 formdata = {
@@ -20,9 +43,8 @@ formdata = {
     'minimum_severity': 'Info',
     'active': 'true',
     'verified':'true',
-    'scan_type':'Bandit Scan',
-    'engagement':'1',
-    'lead':'1',
+    'scan_type':scanner,
+    'engagement':engagement_id,
     'push_to_jira':'false',
     'close_old_findings':'false'
 }
@@ -31,7 +53,8 @@ formdata = {
 print(headers)
 print(formdata)
 
-r = requests.post(url, headers=headers, data=formdata, files=files) 
 
-for item in r:
-   print(item)
+
+
+
+
